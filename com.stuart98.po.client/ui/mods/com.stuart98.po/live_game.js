c1380e4af0f8b2b3782d626cdea16d4171e49bee
Stuart Hepworth <robostuart@gmail.com>
2016-10-09 11:58:13 -0600

    Fixed bugs.

--- com.stuart98.po.client/ui/mods/com.stuart98.po/live_game.js
+++ com.stuart98.po.client/ui/mods/com.stuart98.po/live_game.js
@@ -15,14 +15,14 @@ if ( ! POLoaded )
 
         if (model.ammoBuildHover)
         {
-            model.ammoBuildHover["/pa/units/land/l_anti_nuke_launcher/l_anti_nuke_launcher_ammo.json"] = {
+            model.ammoBuildHover["/pa/units/legion/land/l_anti_nuke_launcher/l_anti_nuke_launcher_ammo.json"] = {
                 name: 'Iron Dome Interceptor',
                 description: 'Anti-nuke - Intercepts incoming nuclear missiles.',
                 cost: 5000,
                 sicon_override: 'anti_nuke_launcher_ammo',
                 damage: 1
             };
-            model.ammoBuildHover["/pa/units/land/l_nuke_launcher/l_nuke_launcher_ammo.json"] = {
+            model.ammoBuildHover["/pa/units/legion/land/l_nuke_launcher/l_nuke_launcher_ammo.json"] = {
                 name: 'Supernova Strategic Warhead',
                 description: 'Long range, high damage ballistic missile.',
                 cost: 30000,
